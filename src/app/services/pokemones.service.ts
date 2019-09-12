822053d42122ecad84392dac3a8fbdc7bdd5b1e5
Victor <g.victor.juan@gmail.com>
2019-09-12 08:25:57 -0300

    documentado el servicio de pokemones

--- src/app/services/pokemones.service.ts
+++ src/app/services/pokemones.service.ts
@@ -10,27 +10,31 @@ export class PokemonesService {
 
   private url = 'https://generatedatarankpvpgo.firebaseio.com';
 
-  // Bases STATS
-  private BaseAtk: number;
-  private BaseDef: number;
-  private BaseStam: number;
-
-  // IVs STATS
-  private AtkIV: number;
-  private DefIV: number;
-  private StamIV: number;
-
-  // tslint:disable-next-line: variable-name
   constructor(private _httpClient: HttpClient ) { }
 
+  /////////////////////////////////////////////////
+  // Metodos Publicos
+  /////////////////////////////////////////////////
+
+  /**
+   * Borra un pokemon mediante el id de firebase
+   * @param {string} idFirebase
+   */
   borrarPokemon( idFirebase: string ) {
     return this._httpClient.delete(`${this.url}/pokemones/${idFirebase}.json`);
   }
 
-  getPokemonById( idFirebase: string ){
+  /**
+   * Devuelve un pokemon mediante el id de firebase
+   * @param idFirebase
+   */
+  getPokemonById( idFirebase: string ) {
     return this._httpClient.get(`${this.url}/pokemones/${idFirebase}.json`);
   }
 
+  /**
+   * Devuelve todos los pokemones
+   */
   getPokemones() {
     return this._httpClient.get(`${this.url}/pokemones.json`).pipe(
       map( this._crearArrPokemones ), // Forma pipicucu de poner lo mismo
@@ -38,6 +42,47 @@ export class PokemonesService {
     );
   }
 
+  /**
+   * Crea un pokemon en firebase
+   * @param {PokemonModel} poke
+   */
+  crearPokemon(poke: PokemonModel) {
+    const url = `${this.url}/pokemones.json`;
+
+    return this._httpClient.post(url, poke)
+      .pipe(
+        map((resp: any) => {
+          console.log(resp);
+          poke.idFirebase = resp.name;
+          return poke;
+        })
+      );
+  }
+
+  /**
+   * Actualiza un pokemon en especifico
+   * @param {PokemonModel} poke
+   */
+  actualizarPokemon(poke: PokemonModel) {
+    const pokeTemp = {
+      ...poke
+    };
+
+    delete pokeTemp.idFirebase;
+
+    const url = `${this.url}/pokemones/${poke.idFirebase}.json`;
+
+    return this._httpClient.put(url, pokeTemp);
+  }
+
+  /////////////////////////////////////////////////
+  // Metodos Privados
+  /////////////////////////////////////////////////
+
+  /**
+   * Carpinteria del response de firebase para obtener un arreglo de pokemones
+   * @param pokemonesObj
+   */
   private _crearArrPokemones(pokemonesObj: object) {
     const pokes: PokemonModel[] = [];
 
@@ -52,82 +97,5 @@ export class PokemonesService {
 
     return pokes;
   }
-
-  crearPokemon( poke: PokemonModel) {
-    const url = `${this.url}/pokemones.json`;
-
-    return this._httpClient.post(url, poke)
-              .pipe(
-                map( (resp: any) => {
-                  console.log(resp);
-                  poke.idFirebase = resp.name;
-                  return poke;
-                })
-              );
-  }
-
-  actualizarPokemon( poke: PokemonModel ) {
-
-    const pokeTemp = {
-      ...poke
-    };
-
-    delete pokeTemp.idFirebase;
-
-    const url = `${this.url}/pokemones/${ poke.idFirebase }.json`;
-
-    return this._httpClient.put(url, pokeTemp);
-  }
-
-  getLevel(): number {
-    if (this._getM12() === 1500) {
-      const aux = this._getO12() + 0.5;
-      const aux2 = 40;
-
-      if (aux >= aux2) {
-        return aux2;
-      } else {
-        return aux;
-      }
-
-    } else {
-      return this._getO12();
-    }
-
-    return -1;
-  }
-
-  _getO12(): number {
-    return -1;
-  }
-
-
-
-
-
-  _getQ12(): number {
-    return -1;
-  }
-
-  _getM12(): number {
-    // tslint:disable-next-line: no-bitwise
-    return ((15000 / this._getCPAux()) ^ 0.5);
-  }
-
-
-
-  _getCPAux(): number {
-    // tslint:disable-next-line: no-bitwise
-    return ((this.BaseAtk + this.AtkIV) * ((this.BaseDef + this.DefIV) ^ 0.5) * ((this.BaseStam + this.StamIV) ^ 0.5));
-  }
-
-  _buscar(): number {
-    return 0;
-  }
-
-
-
-
-
 
 }
